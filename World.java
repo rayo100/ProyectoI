4640dac6cd14bc059827bfe9a879fb20adbaa279
ronis97 <ronivillarreal@hotmail.com>
2021-08-29 17:18:16 -0500

    cambiosAdicionales

--- World.java
+++ World.java
@@ -15,5 +15,40 @@ public class World
     public void addRoute(String locationA, String locationB, int cost){
     
     }
+    public void putArmy(String location){
     
+    }
+    public void delNation(String color){
+    
+    }
+    public void delStreet(String locationA, String locationB){
+    
+    }
+    public void removeArmy(String location){
+    
+    }
+    public void moveArmyOneRoute(String locationA, String locationB){
+    
+    }
+    public String[] conqueredNations(){
+        return null;
+    }
+    public int payments(){
+        return 0;
+    }
+    public boolean conquer(){
+        return false;
+    }
+    public void makeVisible(){
+    
+    }
+    public void makeInvisible(){
+    
+    }
+    public void finis(){
+    
+    }
+    public boolean ok(){
+        return false;
+    }
 }
